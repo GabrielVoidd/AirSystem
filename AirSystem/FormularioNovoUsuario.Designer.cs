2c6ec212936af913ecd0e97b195fcf8ef764494a
GabrielVoidd <souzagabriel0006@gmail.com>
2020-06-14 22:15:58 -0300

    Criação da tela de cadastro

---
+++ AirSystem/FormularioNovoUsuario.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace AirSystem
+{
+    partial class FormularioNovoUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nome = new System.Windows.Forms.Label();
+            this.Endereco = new System.Windows.Forms.Label();
+            this.DataNascimento = new System.Windows.Forms.Label();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Senha = new System.Windows.Forms.Label();
+            this.ConfirmarSenha = new System.Windows.Forms.Label();
+            this.TextBoxConfirmarSenha = new System.Windows.Forms.TextBox();
+            this.TextBoxSenha = new System.Windows.Forms.TextBox();
+            this.TextBoxUsuario = new System.Windows.Forms.TextBox();
+            this.TextBoxEndereco = new System.Windows.Forms.TextBox();
+            this.TextBoxNome = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.TextBoxSobrenome = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TextBoxNumero = new System.Windows.Forms.TextBox();
+            this.BotaoAlterarFoto = new System.Windows.Forms.Button();
+            this.BotaoDeletarFoto = new System.Windows.Forms.Button();
+            this.EscolhaAdmin = new System.Windows.Forms.CheckBox();
+            this.BotaoCadastrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nome.Location = new System.Drawing.Point(27, 48);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(45, 19);
+            this.Nome.TabIndex = 0;
+            this.Nome.Text = "Nome";
+            // 
+            // Endereco
+            // 
+            this.Endereco.AutoSize = true;
+            this.Endereco.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Endereco.Location = new System.Drawing.Point(27, 90);
+            this.Endereco.Name = "Endereco";
+            this.Endereco.Size = new System.Drawing.Size(81, 19);
+            this.Endereco.TabIndex = 1;
+            this.Endereco.Text = "Endereço";
+            // 
+            // DataNascimento
+            // 
+            this.DataNascimento.AutoSize = true;
+            this.DataNascimento.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DataNascimento.Location = new System.Drawing.Point(27, 133);
+            this.DataNascimento.Name = "DataNascimento";
+            this.DataNascimento.Size = new System.Drawing.Size(99, 19);
+            this.DataNascimento.TabIndex = 2;
+            this.DataNascimento.Text = "Nascimento";
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(27, 178);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(72, 19);
+            this.Usuario.TabIndex = 3;
+            this.Usuario.Text = "Usuário";
+            // 
+            // Senha
+            // 
+            this.Senha.AutoSize = true;
+            this.Senha.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Senha.Location = new System.Drawing.Point(27, 225);
+            this.Senha.Name = "Senha";
+            this.Senha.Size = new System.Drawing.Size(54, 19);
+            this.Senha.TabIndex = 4;
+            this.Senha.Text = "Senha";
+            // 
+            // ConfirmarSenha
+            // 
+            this.ConfirmarSenha.AutoSize = true;
+            this.ConfirmarSenha.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConfirmarSenha.Location = new System.Drawing.Point(27, 271);
+            this.ConfirmarSenha.Name = "ConfirmarSenha";
+            this.ConfirmarSenha.Size = new System.Drawing.Size(144, 19);
+            this.ConfirmarSenha.TabIndex = 5;
+            this.ConfirmarSenha.Text = "Confirmar senha";
+            // 
+            // TextBoxConfirmarSenha
+            // 
+            this.TextBoxConfirmarSenha.Location = new System.Drawing.Point(195, 270);
+            this.TextBoxConfirmarSenha.Name = "TextBoxConfirmarSenha";
+            this.TextBoxConfirmarSenha.PasswordChar = '*';
+            this.TextBoxConfirmarSenha.Size = new System.Drawing.Size(169, 20);
+            this.TextBoxConfirmarSenha.TabIndex = 6;
+            // 
+            // TextBoxSenha
+            // 
+            this.TextBoxSenha.Location = new System.Drawing.Point(195, 223);
+            this.TextBoxSenha.Name = "TextBoxSenha";
+            this.TextBoxSenha.PasswordChar = '*';
+            this.TextBoxSenha.Size = new System.Drawing.Size(169, 20);
+            this.TextBoxSenha.TabIndex = 7;
+            // 
+            // TextBoxUsuario
+            // 
+            this.TextBoxUsuario.Location = new System.Drawing.Point(195, 176);
+            this.TextBoxUsuario.Name = "TextBoxUsuario";
+            this.TextBoxUsuario.Size = new System.Drawing.Size(169, 20);
+            this.TextBoxUsuario.TabIndex = 8;
+            // 
+            // TextBoxEndereco
+            // 
+            this.TextBoxEndereco.Location = new System.Drawing.Point(195, 88);
+            this.TextBoxEndereco.Name = "TextBoxEndereco";
+            this.TextBoxEndereco.Size = new System.Drawing.Size(202, 20);
+            this.TextBoxEndereco.TabIndex = 10;
+            // 
+            // TextBoxNome
+            // 
+            this.TextBoxNome.Location = new System.Drawing.Point(195, 46);
+            this.TextBoxNome.Name = "TextBoxNome";
+            this.TextBoxNome.Size = new System.Drawing.Size(93, 20);
+            this.TextBoxNome.TabIndex = 11;
+            this.TextBoxNome.Text = "Digite seu nome";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(195, 131);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(169, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // TextBoxSobrenome
+            // 
+            this.TextBoxSobrenome.Location = new System.Drawing.Point(313, 46);
+            this.TextBoxSobrenome.Name = "TextBoxSobrenome";
+            this.TextBoxSobrenome.Size = new System.Drawing.Size(169, 20);
+            this.TextBoxSobrenome.TabIndex = 13;
+            this.TextBoxSobrenome.Text = "Digite seu sobrenome";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(558, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(212, 170);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TextBoxNumero
+            // 
+            this.TextBoxNumero.Location = new System.Drawing.Point(428, 88);
+            this.TextBoxNumero.Name = "TextBoxNumero";
+            this.TextBoxNumero.Size = new System.Drawing.Size(54, 20);
+            this.TextBoxNumero.TabIndex = 15;
+            this.TextBoxNumero.Text = "Nº";
+            // 
+            // BotaoAlterarFoto
+            // 
+            this.BotaoAlterarFoto.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotaoAlterarFoto.Location = new System.Drawing.Point(558, 236);
+            this.BotaoAlterarFoto.Name = "BotaoAlterarFoto";
+            this.BotaoAlterarFoto.Size = new System.Drawing.Size(104, 23);
+            this.BotaoAlterarFoto.TabIndex = 16;
+            this.BotaoAlterarFoto.Text = "Alterar";
+            this.BotaoAlterarFoto.UseVisualStyleBackColor = true;
+            // 
+            // BotaoDeletarFoto
+            // 
+            this.BotaoDeletarFoto.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotaoDeletarFoto.Location = new System.Drawing.Point(666, 236);
+            this.BotaoDeletarFoto.Name = "BotaoDeletarFoto";
+            this.BotaoDeletarFoto.Size = new System.Drawing.Size(104, 23);
+            this.BotaoDeletarFoto.TabIndex = 17;
+            this.BotaoDeletarFoto.Text = "Deletar";
+            this.BotaoDeletarFoto.UseVisualStyleBackColor = true;
+            // 
+            // EscolhaAdmin
+            // 
+            this.EscolhaAdmin.AutoSize = true;
+            this.EscolhaAdmin.Location = new System.Drawing.Point(558, 298);
+            this.EscolhaAdmin.Name = "EscolhaAdmin";
+            this.EscolhaAdmin.Size = new System.Drawing.Size(65, 17);
+            this.EscolhaAdmin.TabIndex = 18;
+            this.EscolhaAdmin.Text = "É Admin";
+            this.EscolhaAdmin.UseVisualStyleBackColor = true;
+            // 
+            // BotaoCadastrar
+            // 
+            this.BotaoCadastrar.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotaoCadastrar.Location = new System.Drawing.Point(235, 323);
+            this.BotaoCadastrar.Name = "BotaoCadastrar";
+            this.BotaoCadastrar.Size = new System.Drawing.Size(82, 23);
+            this.BotaoCadastrar.TabIndex = 19;
+            this.BotaoCadastrar.Text = "Cadastrar";
+            this.BotaoCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // FormularioNovoUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BotaoCadastrar);
+            this.Controls.Add(this.EscolhaAdmin);
+            this.Controls.Add(this.BotaoDeletarFoto);
+            this.Controls.Add(this.BotaoAlterarFoto);
+            this.Controls.Add(this.TextBoxNumero);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TextBoxSobrenome);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.TextBoxNome);
+            this.Controls.Add(this.TextBoxEndereco);
+            this.Controls.Add(this.TextBoxUsuario);
+            this.Controls.Add(this.TextBoxSenha);
+            this.Controls.Add(this.TextBoxConfirmarSenha);
+            this.Controls.Add(this.ConfirmarSenha);
+            this.Controls.Add(this.Senha);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.DataNascimento);
+            this.Controls.Add(this.Endereco);
+            this.Controls.Add(this.Nome);
+            this.Name = "FormularioNovoUsuario";
+            this.Text = "Novo usuário - AirSystem";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label Endereco;
+        private System.Windows.Forms.Label DataNascimento;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label Senha;
+        private System.Windows.Forms.Label ConfirmarSenha;
+        private System.Windows.Forms.TextBox TextBoxConfirmarSenha;
+        private System.Windows.Forms.TextBox TextBoxSenha;
+        private System.Windows.Forms.TextBox TextBoxUsuario;
+        private System.Windows.Forms.TextBox TextBoxEndereco;
+        private System.Windows.Forms.TextBox TextBoxNome;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox TextBoxSobrenome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TextBoxNumero;
+        private System.Windows.Forms.Button BotaoAlterarFoto;
+        private System.Windows.Forms.Button BotaoDeletarFoto;
+        private System.Windows.Forms.CheckBox EscolhaAdmin;
+        private System.Windows.Forms.Button BotaoCadastrar;
+    }
+}
